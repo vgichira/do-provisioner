0f93b3322abae3f1c528f1b07a48585a170778b6
Vincent Muchiri <vgichira39@gmail.com>
2023-12-01 22:20:32 +0300

    set up staging environment

---
+++ live/staging/main.tf
@@ -0,0 +1,6 @@
+module "droplet" {
+  source         = "../../modules/digitalocean/droplet"
+  droplet_count  = 3
+  image          = "ubuntu-20-04-x64"
+  size           = "s-1vcpu-1gb"
+}
---
+++ live/staging/variables.tf
@@ -0,0 +1,9 @@
+variable "do_token" {
+  type = string
+  description = "Personal access token to authenticate Digital Ocean APIs"
+}
+
+variable "tags" {
+    type = set(string)
+    default = ["staging"]
+}
